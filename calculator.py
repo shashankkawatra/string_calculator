d666c078faf7efbf00adc98790530dbce8af958f
Shashank Kawatra <shashankawatra@SHASHANKS-MACBOOK-PRO.local>
2025-06-16 15:19:00 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+
+# Empty string returns 0
+def add(numbers):
+    if numbers == "":
+        return 0
+
---
+++ test_calculator.py
@@ -0,0 +1,5 @@
+from calculator import add
+
+# Empty string returns 0
+def test_empty_string_returns_zero():
+    assert add("") == 0
