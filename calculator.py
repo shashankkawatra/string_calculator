9738422e3d4a8a874d6a581da68cee1a0cbd66c9
Shashank Kawatra <shashankawatra@SHASHANKS-MACBOOK-PRO.local>
2025-06-16 15:23:52 +0530

    update code for single number

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 
 # Empty string returns 0
+# Single number returns its value
+
 def add(numbers):
     if numbers == "":
         return 0
-
+    return int(numbers)
--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,7 @@ from calculator import add
 # Empty string returns 0
 def test_empty_string_returns_zero():
     assert add("") == 0
+
+# Single number returns its value
+def test_single_number_returns_its_value():
+    assert add("1") == 1
